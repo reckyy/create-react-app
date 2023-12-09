e056832a01879349f9c8329eb249cbc09c7ef412
reckyy <srreki98@gmail.com>
2023-12-09 22:36:19 +0900

    ⚡️ feat: 詳細機能実装

--- src/List.js
+++ src/List.js
@@ -13,6 +13,9 @@ let nextId = 3;
 export default function List() {
   const [memos, setMemos] = useState(initialMemos);
   const [answer, setAnswer] = useState("");
+  const [editable, setEditable] = useState(false);
+  const [editingMemoId, setEditingMemoId] = useState(null);
+
   const firstRowOfMemos = memos.map(({ id, content }) => ({
     id: id,
     value: content.split("\n")[0],
@@ -29,17 +32,52 @@ export default function List() {
     setAnswer("");
   }
 
-  return (
+  function handleSaveMemo(e) {
+    e.preventDefault();
+    const nextMemos = memos.map((memo) => {
+      if (memo.id === editingMemoId) {
+        return { id: memo.id, content: answer };
+      } else {
+        return memo;
+      }
+    });
+    setMemos(nextMemos);
+    setAnswer("");
+    setEditable(false);
+    setEditingMemoId(null);
+  }
+
+  function handleMemoEditable(memo) {
+    const targetMemo = memos.find((m) => m.id === memo.id);
+    setAnswer(targetMemo.content);
+    setEditingMemoId(targetMemo.id);
+    setEditable(true);
+  }
+
+  return editable ? (
+    <>
+      <h1>メモ詳細</h1>
+      <Form
+        value={answer}
+        handleAnswerChange={handleAnswerChange}
+        handleAddMemo={handleSaveMemo}
+        buttonText={"save"}
+      />
+    </>
+  ) : (
     <>
       <h1>メモ一覧</h1>
       <Form
         value={answer}
         handleAnswerChange={handleAnswerChange}
         handleAddMemo={handleAddMemo}
+        buttonText={"add"}
       />
       <ul>
         {firstRowOfMemos.map((memo) => (
-          <li key={memo.id}>{memo.value}</li>
+          <li key={memo.id} onClick={() => handleMemoEditable(memo)}>
+            {memo.value}
+          </li>
         ))}
         +
       </ul>
