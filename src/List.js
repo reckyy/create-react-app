ec9492471162b68b18a4527de802d72a9f7ac085
reckyy <srreki98@gmail.com>
2023-12-10 12:07:47 +0900

    ⚡️ feat: 画面遷移をstatusで管理

--- src/List.js
+++ src/List.js
@@ -8,8 +8,12 @@ let initialMemos = JSON.parse(localStorage.getItem("memos")) || [];
 export default function List() {
   const [memos, setMemos] = useState(initialMemos);
   const [answer, setAnswer] = useState("");
-  const [editable, setEditable] = useState(false);
+  const [status, setStatus] = useState('index');
   const [editingMemoId, setEditingMemoId] = useState(null);
+
+  const index = status === 'index';
+  const isAdding = status === 'add';
+  const isEditing = status === 'edit';
 
   useEffect(() => {
     localStorage.setItem("memos", JSON.stringify(memos));
@@ -24,15 +28,20 @@ export default function List() {
     setAnswer(e.target.value);
   }
 
-  function handleAddMemo(e) {
+  function handleChangeStatusToAdd(){
+    setStatus('add');
+  }
+
+  async function handleAddMemo(e) {
     e.preventDefault();
     const nextId = memos.length > 0 ? memos[memos.length - 1].id + 1 : 0;
     const nextMemos = [...memos, { id: nextId, content: answer }];
-    setMemos(nextMemos);
-    setAnswer("");
+    await setMemos(nextMemos);
+    await setAnswer("");
+    setStatus('index');
   }
 
-  function handleSaveMemo(e) {
+  async function handleSaveMemo(e) {
     e.preventDefault();
     const nextMemos = memos.map((memo) => {
       if (memo.id === editingMemoId) {
@@ -41,28 +50,52 @@ export default function List() {
         return memo;
       }
     });
-    setMemos(nextMemos);
-    setAnswer("");
-    setEditable(false);
-    setEditingMemoId(null);
+    await setMemos(nextMemos);
+    await setAnswer("");
+    await setEditingMemoId(null);
+    setStatus('index');
   }
 
   function handleMemoEditable(memo) {
     const targetMemo = memos.find((m) => m.id === memo.id);
     setAnswer(targetMemo.content);
     setEditingMemoId(targetMemo.id);
-    setEditable(true);
+    setStatus('edit');
   }
 
   function handleDeleteMemo(memo) {
     setMemos(memos.filter((memo) => memo.id !== editingMemoId));
     setAnswer("");
-    setEditable(false);
+    setStatus('index');
     setEditingMemoId(null);
   }
 
-  return editable ? (
+  if (index) {return(
     <div class="container">
+    <h1>メモ一覧</h1>
+    <ul>
+      {firstRowOfMemos.map((memo) => (
+        <li key={memo.id} onClick={() => handleMemoEditable(memo)}>
+          {memo.value}
+        </li>
+      ))}
+      <li onClick={handleChangeStatusToAdd}>+</li>
+    </ul>
+  </div>
+  );
+  } else if(isAdding) {return(
+    <div class="container">
+      <h1>新規メモ</h1>
+      <Input
+      value={answer}
+      handleAnswerChange={handleAnswerChange}
+      />
+      <button type="submit" onClick={handleAddMemo}>add</button>
+    </div>
+  );
+  } else if(isEditing) {
+    return(
+      <div class="container">
       <h1>メモ詳細</h1>
       <Input
         value={answer}
@@ -71,22 +104,6 @@ export default function List() {
       <button onClick={handleSaveMemo}>save</button>
       <button onClick={handleDeleteMemo}>delete</button>
     </div>
-  ) : (
-    <div class="container">
-      <h1>メモ一覧</h1>
-      <Input
-        value={answer}
-        handleAnswerChange={handleAnswerChange}
-      />
-      <button type="submit" onClick={handleAddMemo}>add</button>
-      <ul>
-        {firstRowOfMemos.map((memo) => (
-          <li key={memo.id} onClick={() => handleMemoEditable(memo)}>
-            {memo.value}
-          </li>
-        ))}
-        +
-      </ul>
-    </div>
-  );
+    );
+  }
 }
