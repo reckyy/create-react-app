d8e9df163f04f2d2098bc5862fc715314033f6d0
reckyy <srreki98@gmail.com>
2024-01-18 22:53:54 +0900

    📃 chore: 拡張子変更

--- src/components/NewMemo.jsx
+++ src/components/NewMemo.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Input from "../components/Input";
+import Input from "./Input";
 import "../css/list.css";
 
 export default function NewMemo({ memos, onAdd }) {
